5fd344bb5f60c9e17ceb84f89802504374287601
Tural Taghiyev <turalrt@code.edu.az>
2021-11-28 23:15:38 +0400

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 8;
+int i;
+int count=0;
+
+for (i=1; i<=n; i++)
+{
+    if (n % i==0)
+    {
+        count++;
+    }
+}
+if (count == 2)
+    Console.WriteLine("n is prime");
+else
+    Console.WriteLine("n is not prime");
